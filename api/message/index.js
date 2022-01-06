e642c311fa8ace3fe51418cbf09f7ab37dc6a404
jhealy <jhealy@microsoft.com>
2022-01-06 11:26:28 -0500

    api added

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,3 @@
 module.exports = async function (context, req) {
-    context.res.json({
-        text: "hello from dfvanilla message api"
-    });
-}
+    context.res.json( { text : "Hello from the API" } );
+};
